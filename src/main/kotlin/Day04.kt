d3b3954e45e479130b67a27848c3d165fc9fb80a
marazmarci <marazmarci@gmail.com>
2022-12-08 00:20:44 +0100

    Day04/2 solution

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -4,19 +4,30 @@
 fun main() {
     val inputLines = readInputFileLines(4).filter { it.isNotBlank() }
 
-    val count = inputLines.map { line ->
+    val rangePairs = inputLines.map { line ->
         val rangeStrList = line.split(',')
         val (firstRange, secondRange) = rangeStrList.map {
             val (rangeStart, rangeEnd) = it.split('-')
             rangeStart.toInt()..rangeEnd.toInt()
         }
         firstRange to secondRange
-    }.count { (firstRange, secondRange) ->
+    }
+
+    val countPart1 = rangePairs.count { (firstRange, secondRange) ->
         firstRange in secondRange || secondRange in firstRange
     }
-    println(count)
-    check(count == 464)
+    println(countPart1)
+    check(countPart1 == 464)
 
+    // part 2:
+    val countPart2 = rangePairs.count { (firstRange, secondRange) ->
+        firstRange.overlaps(secondRange)
+    }
+    println(countPart2)
+    check(countPart2 == 770)
 }
 
 private operator fun IntRange.contains(other: IntRange) = other.first in this && other.last in this
+
+private fun IntRange.overlaps(other: IntRange) =
+    other.first in this || other.last in this || first in other || last in other
