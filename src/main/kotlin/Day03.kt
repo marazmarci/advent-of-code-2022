5923003ad63e1c0fe120eab4696eda214de00461
marazmarci <marazmarci@gmail.com>
2022-12-07 23:44:34 +0100

    Day03/2 solution

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -2,24 +2,34 @@
  * https://adventofcode.com/2022/day/3
  */
 fun main() {
-    val inputLines = readInputFileLines(3)
+    val inputLines = readInputFileLines(3).filterNot { it.isBlank() }
 
-    val sumOfPriorities = inputLines
-        .filterNot {
-            it.isBlank()
-        }.map {
-            check(it.length.mod(2) == 0)
-            val halfLength = it.length / 2
-            val firstHalf = it.take(halfLength).toList()
-            val secondHalf = it.takeLast(halfLength).toList()
-            val intersection = firstHalf.intersect(secondHalf.toSet())
-            val commonItem = intersection.single()
-            commonItem
+    // region Part 1
+    val sumOfPriorities1 = inputLines.map {
+        check(it.length.mod(2) == 0)
+        val halfLength = it.length / 2
+        val firstHalf = it.take(halfLength).toList()
+        val secondHalf = it.takeLast(halfLength).toList()
+        val intersection = firstHalf.intersect(secondHalf.toSet())
+        intersection.single()
+    }.map {
+        it.priority
+    }.sum()
+    println(sumOfPriorities1)
+    // endregion Part 1
+
+    // region Part 2
+    val sumOfPriorities2 = inputLines.chunked(3)
+        .map { rucksacks ->
+            val (first, second, third) = rucksacks.map { it.toList() }
+            val intersection = first.intersect(second.toSet()).intersect(third.toSet())
+            intersection.single()
         }.map {
             it.priority
         }.sum()
+    println(sumOfPriorities2)
+    // endregion Part 2
 
-    println(sumOfPriorities)
 }
 
 private val Char.priority
